5768960dea558e634258c6ad8262212b7f00f999
Victor Milovanov <lost@losttech.software>
2020-10-14 10:47:36 -0700

    added HResult.RPC_E_CANTCALLOUT_ININPUTSYNCCALL

--- source/VirtualDesktop/Interop/HResult.cs
+++ source/VirtualDesktop/Interop/HResult.cs
@@ -6,6 +6,11 @@ namespace WindowsDesktop.Interop
 {
 	public enum HResult : uint
 	{
+		/// <summary>
+		/// You can not call shell functions from a WndProc handler,
+		/// when the message needs synchronous processing (e.g. sent by SendMessage).
+		/// </summary>
+		RPC_E_CANTCALLOUT_ININPUTSYNCCALL = 0x8001010D,
 		TYPE_E_OUTOFBOUNDS = 0x80028CA1,
 		TYPE_E_ELEMENTNOTFOUND = 0x8002802B,
 		REGDB_E_CLASSNOTREG = 0x80040154,
